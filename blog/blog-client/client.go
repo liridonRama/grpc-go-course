162b6a57042a1cc1ae9aa250c26afd8e3d573a20
liridon.rama <liridon.rama@me.com>
2022-01-02 09:35:09 +0100

    adjust client

--- blog/blog-client/client.go
+++ blog/blog-client/client.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"time"
 
 	"github.com/liridonrama/grpc-go-course/blog/blogpb"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
@@ -20,55 +22,55 @@ func main() {
 
 	c := blogpb.NewBlogServiceClient(cc)
 
-	// req := &blogpb.CreateBlogRequest{
-	// 	Blog: &blogpb.Blog{
-	// 		Id:       "",
-	// 		AuthorId: primitive.NewObjectID().Hex(),
-	// 		Title:    "Some title",
-	// 		Content:  "Some content",
-	// 	},
-	// }
+	req := &blogpb.CreateBlogRequest{
+		Blog: &blogpb.Blog{
+			Id:       "",
+			AuthorId: primitive.NewObjectID().Hex(),
+			Title:    "Some title",
+			Content:  "Some content",
+		},
+	}
 
-	// res, err := c.CreateBlog(context.Background(), req)
-	// if err != nil {
-	// 	log.Panicln("Error while getting response from server", err)
-	// }
+	res, err := c.CreateBlog(context.Background(), req)
+	if err != nil {
+		log.Panicln("Error while getting response from server", err)
+	}
 
-	// fmt.Println("Blog has been created:", res.GetBlog())
+	fmt.Println("Blog has been created:", res.GetBlog())
 
-	// rBReq := &blogpb.ReadBlogRequest{
-	// 	BlogId: "61cf25475edd00e9eac3ef95",
-	// }
+	rBReq := &blogpb.ReadBlogRequest{
+		BlogId: "61cf25475edd00e9eac3ef95",
+	}
 
-	// rBRes, err := c.ReadBlog(context.Background(), rBReq)
-	// if err != nil {
-	// 	log.Panicln("Error while getting response from server", err)
-	// }
+	rBRes, err := c.ReadBlog(context.Background(), rBReq)
+	if err != nil {
+		log.Println("Error while getting response from server", err)
+	} else {
+		fmt.Println("Blog has been retrieved:", rBRes.GetBlog())
+	}
 
-	// fmt.Println("Blog has been retrieved:", rBRes.GetBlog())
+	uBReq := &blogpb.UpdateBlogRequest{
+		Blog: &blogpb.Blog{
+			Id:      "61cf25475edd00e9eac3ef95",
+			Content: "ueli, hans mauerer",
+		},
+	}
 
-	// uBReq := &blogpb.UpdateBlogRequest{
-	// 	Blog: &blogpb.Blog{
-	// 		Id:      "61cf25475edd00e9eac3ef95",
-	// 		Content: fmt.Sprintf("ueli, hans mauerer %v", strconv.Itoa(rand.Int())),
-	// 	},
-	// }
+	uBRes, err := c.UpdateBlog(context.Background(), uBReq)
+	if err != nil {
+		log.Println("Error while getting response from server", err)
+	} else {
+		fmt.Println("Blog has been updated:", uBRes.GetBlog())
+	}
 
-	// uBRes, err := c.UpdateBlog(context.Background(), uBReq)
-	// if err != nil {
-	// 	log.Panicln("Error while getting response from server", err)
-	// }
-
-	// fmt.Println("Blog has been updated:", uBRes.GetBlog())
-
-	// dBRes, err := c.DeleteBlog(context.Background(), &blogpb.DeleteBlogRequest{
-	// 	BlogId: "61d00f562023fb557458d377",
-	// })
-	// if err != nil {
-	// 	log.Panicln("Error while getting response from server", err)
-	// }
-
-	// fmt.Println("Blog has been deleted:", dBRes.GetBlogId())
+	dBRes, err := c.DeleteBlog(context.Background(), &blogpb.DeleteBlogRequest{
+		BlogId: "61d00f562023fb557458d377",
+	})
+	if err != nil {
+		log.Println("Error while getting response from server", err)
+	} else {
+		fmt.Println("Blog has been deleted:", dBRes.GetBlogId())
+	}
 
 	// list blogs
 	rStream, err := c.ListBlog(context.Background(), &blogpb.ListBlogRequest{})
@@ -87,6 +89,8 @@ func main() {
 		}
 
 		fmt.Println("blog reveiced:", blogM.GetBlog())
+
+		time.Sleep(time.Second)
 	}
 
 	fmt.Println("All Blogs retrieved")
